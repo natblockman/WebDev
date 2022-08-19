74b5a9e20b41e4a737ce0fb345943d8cb344627b
natblockman <natblockman@gmail.com>
2022-08-19 21:31:24 +0700

    mongodb-end

---
+++ mywebsite/nattapatjantarasugree/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-success">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">Nattapat Jantarasugree</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+   
+      </div>
+      <div>
+      <h1>Welcome to my website</h1>
+      </div>
+   
+    </div>
+  );
+}
+
+export default App;
